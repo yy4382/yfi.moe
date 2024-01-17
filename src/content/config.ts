7f4b595260df9456f25087738a468673418d0949
Chris Yang <yy4382@outlook.com>
2024-01-17 20:45:03 +0800

    fix: change posts into post

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,19 +2,19 @@
 import { z, defineCollection } from "astro:content";
 // 为每一个集合定义一个 `type` 和 `schema`
 const postsCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-      title: z.string(),
-      date: z.date(),
-      description: z.string().optional(),
-      // author: z.string(),
-      photos: z.string().default('/img/meta/default_cover.webp'),
-      updated: z.date(),
-      // categories: z.array(z.string()),
-      tags: z.array(z.string())
-    })
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    description: z.string().optional(),
+    // author: z.string(),
+    photos: z.string().default("/img/meta/default_cover.webp"),
+    updated: z.date(),
+    // categories: z.array(z.string()),
+    tags: z.array(z.string()),
+  }),
 });
 // 导出一个单独的 `collections` 对象来注册你的集合
 export const collections = {
-  posts: postsCollection,
+  post: postsCollection,
 };
