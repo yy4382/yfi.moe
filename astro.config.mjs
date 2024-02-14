038048c10edf0071d34b590abf519a8f569633d8
Chris Yang <yy4382@outlook.com>
2024-02-14 12:24:58 +0800

    feat: search func

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,18 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 import remarkGithubAlerts from "remark-github-alerts";
-
+import pagefind from "astro-pagefind";
 import icon from "astro-icon";
 import remarkReadingTime from "./src/utils/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.yfi.moe/",
-  integrations: [tailwind(), vue(), icon()],
+  integrations: [tailwind(), vue(), icon(), pagefind()],
   redirects: {
     "/": "/1",
   },
   markdown: {
-    remarkPlugins: [remarkGithubAlerts,remarkReadingTime],
+    remarkPlugins: [remarkGithubAlerts, remarkReadingTime],
   },
 });
