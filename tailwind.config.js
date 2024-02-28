a0cac254e785946f10508cd9bbc62e1ade908568
Chris Yang <yy4382@outlook.com>
2024-02-28 19:57:37 +0800

    feat: remove custom theme control

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: ["class"],
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
