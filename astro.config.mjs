f210fb88ec56c4d37a4d1a2c80c2bcddb0b3d38b
Chris Yang <yy4382@outlook.com>
2024-03-04 20:59:42 +0800

    chore: add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,75 +7,64 @@ import icon from "astro-icon";
 import remarkReadingTime from "./src/utils/remark-reading-time.mjs";
 import rehypeExtendedLinks from "rehype-extended-links";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.yfi.moe/",
-  integrations: [tailwind(), vue(), icon(), pagefind()],
+  integrations: [tailwind(), vue(), icon(), pagefind(), partytown()],
   redirects: {
-    "/": "/1",
+    "/": "/1"
   },
   markdown: {
     remarkPlugins: [remarkGithubAlerts, remarkReadingTime],
-    rehypePlugins: [
-      [
-        rehypeExtendedLinks,
-        {
-          preContent(node) {
-            const url = node.properties.href;
-            if (!url) return undefined;
-            const regex = /^(https?:\/\/)?(www\.)?github\.com\/.*/i;
-            if (!regex.test(url)) return undefined;
-            return {
-              type: "element",
-              tagName: "span",
-              properties: {
-                className: ["rh-pre-content"],
-              },
-              children: [
-                {
-                  type: "element",
-                  tagName: "svg",
-                  properties: {},
-                  children: [
-                    {
-                      type: "element",
-                      tagName: "use",
-                      properties: {
-                        href: "#github-icon",
-                      },
-                      children: [],
-                    },
-                  ],
-                },
-              ],
-            };
+    rehypePlugins: [[rehypeExtendedLinks, {
+      preContent(node) {
+        const url = node.properties.href;
+        if (!url) return undefined;
+        const regex = /^(https?:\/\/)?(www\.)?github\.com\/.*/i;
+        if (!regex.test(url)) return undefined;
+        return {
+          type: "element",
+          tagName: "span",
+          properties: {
+            className: ["rh-pre-content"]
           },
-          content: {
+          children: [{
             type: "element",
-            tagName: "span",
-            properties: {
-              className: ["rh-post-content"],
-            },
-            children: [
-              {
-                type: "element",
-                tagName: "svg",
-                properties: {},
-                children: [
-                  {
-                    type: "element",
-                    tagName: "use",
-                    properties: {
-                      href: "#external-link-icon",
-                    },
-                    children: [],
-                  },
-                ],
+            tagName: "svg",
+            properties: {},
+            children: [{
+              type: "element",
+              tagName: "use",
+              properties: {
+                href: "#github-icon"
               },
-            ],
-          },
+              children: []
+            }]
+          }]
+        };
+      },
+      content: {
+        type: "element",
+        tagName: "span",
+        properties: {
+          className: ["rh-post-content"]
         },
-      ],
-    ],
-  },
+        children: [{
+          type: "element",
+          tagName: "svg",
+          properties: {},
+          children: [{
+            type: "element",
+            tagName: "use",
+            properties: {
+              href: "#external-link-icon"
+            },
+            children: []
+          }]
+        }]
+      }
+    }]]
+  }
 });
