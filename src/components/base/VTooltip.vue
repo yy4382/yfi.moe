404a5bdd3021ddee19b86992e0cb174fabe54e3a
Chris Yang <yy4382@outlook.com>
2024-03-19 13:51:38 +0800

    refactor: navbar and vtooltip enhanced

--- src/components/base/VTooltip.vue
+++ src/components/base/VTooltip.vue
@@ -1,13 +1,26 @@
 <script setup lang="ts">
 import { ref } from "vue";
 const showTooltip = ref(false);
+const beingHovered = ref(false);
+function mouseEnter() {
+  beingHovered.value = true;
+  showTooltip.value = true;
+}
+function mouseLeave() {
+  beingHovered.value = false;
+  setTimeout(() => {
+    if (!beingHovered.value) showTooltip.value = false;
+  }, 150);
+}
 </script>
 
 <template>
-  <div class="relative">
-    <div @mouseover="showTooltip = true" @mouseout="showTooltip = false">
-      <slot></slot>
-    </div>
+  <div
+    class="relative"
+    @mouseenter="mouseEnter"
+    @mouseleave="mouseLeave"
+  >
+    <slot></slot>
     <div
       class="z-10 absolute top-[calc(100%+0.5rem)] left-1/2 -translate-x-1/2"
     >
@@ -22,7 +35,7 @@ const showTooltip = ref(false);
 <style scoped>
 .v-enter-active,
 .v-leave-active {
-  transition: all 0.3s;
+  transition: all 0.15s;
 }
 .v-enter-from,
 .v-leave-to {
---
+++ src/components/navbar/NavContainer.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import type { NavMenu } from "../../config";
+import { defineProps } from "vue";
+import VTooltip from "../base/VTooltip.vue";
+const props = defineProps<{ nav: NavMenu }>();
+</script>
+<template>
+  <li class="w-20 font-medium justify-center items-center h-full flex">
+    <VTooltip class="self-center h-full">
+      <div class="h-full flex justify-center items-center">
+        <a
+          :href="nav.link"
+          class="w-full hover:text-portage-300 self-center z-1 select-none self-center align-middle"
+        >
+          {{ nav.text }}
+        </a>
+      </div>
+      <template #tooltip>
+        <ul
+          v-if="nav.subMenu && nav.subMenu.length > 0"
+          class="w-24 bg-white dark:bg-gray-900 rounded-lg shadow-lg dark:border-gray-800 p-1 space-y-0.5"
+        >
+          <li
+            v-for="subItem in nav.subMenu"
+            class="text-center rounded-lg py-2 transition-colors hover:(bg-gray-100 dark:bg-gray-800 text-portage-400)"
+          >
+            <a :href="subItem.link" class="text-sm"> {{ subItem.text }} </a>
+          </li>
+        </ul>
+      </template>
+    </VTooltip>
+  </li>
+</template>
