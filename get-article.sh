1adefb184c1a97ae18a83ecde9f932d305dcf497
Chris Yang <yy4382@outlook.com>
2024-04-14 02:30:29 +0800

    chore: move update article script to a single file

---
+++ get-article.sh
@@ -0,0 +1,7 @@
+if [ -d "src/content/post" ]; then
+  cd src/content/post
+  git pull
+  cd ../../..
+else
+  git clone "https://$ARTICLE_PAT@github.com/yy4382/blog-posts.git" --depth 1 --branch main --single-branch --quiet src/content/post 
+fi
