7913e6a1101d917f69cb55865fdbae4b1f80c58d
Chris Yang <yy4382@outlook.com>
2024-04-19 01:55:38 +0800

    fix: search target issue

--- src/components/navbar/SearchPanel.vue
+++ src/components/navbar/SearchPanel.vue
@@ -6,6 +6,7 @@ import {
   AisHighlight,
   AisConfigure,
   AisSnippet,
+  // @ts-ignore
 } from "vue-instantsearch/vue3/es";
 import { algoliaConfig } from "../../config";
 import algoliasearch from "algoliasearch/lite";
@@ -31,7 +32,7 @@ import "instantsearch.css/themes/satellite.css";
         <div
           class="flex flex-col space-y-0.5 whitespace-break-spaces break-all"
         >
-          <a :href="item.slug" class="text-xl">
+          <a :href="'/post/' + item.slug" class="text-xl">
             <AisHighlight attribute="title" :hit="item" />
           </a>
           <AisSnippet attribute="content" :hit="item" />
