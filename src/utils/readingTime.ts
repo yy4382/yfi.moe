84023f330331847a000d6894b73c62861532a4da
Chris Yang <yy4382@outlook.com>
2024-04-22 14:30:20 +0800

    refactor: import path for remarkReadingTime in astro.config.mjs

--- src/utils/readingTime.ts
+++ src/utils/readingTime.ts
@@ -2,7 +2,7 @@ import { toString } from "mdast-util-to-string";
 import getReadingTime from "reading-time";
 
 export default function remarkReadingTime() {
-  return (tree, { data }) => {
+  return (tree: any, { data }: any) => {
     const textOnPage = toString(tree);
     const readingTime = getReadingTime(textOnPage);
     data.astro.frontmatter.minutes = Math.max(
