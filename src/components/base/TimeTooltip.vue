5ea40d4ded177f2cd76d508fff078502664465f6
Chris Yang <yy4382@outlook.com>
2024-06-27 01:41:51 +0800

    refactor: Add TimeTooltip component to display post date and update date
    The PostAttr component has been refactored to include the TimeTooltip component, which displays the post date and update date. This improves the user experience by providing additional information about the timing of the post.

---
+++ src/components/base/TimeTooltip.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import {
+  TooltipContent,
+  TooltipPortal,
+  TooltipProvider,
+  TooltipRoot,
+  TooltipTrigger,
+} from "radix-vue";
+import { Icon } from "@iconify/vue";
+
+defineProps<{
+  postTime: string;
+  updatedTime: string;
+}>();
+</script>
+
+<template>
+  <div>
+    <TooltipProvider>
+      <TooltipRoot>
+        <TooltipTrigger>
+          <slot></slot>
+        </TooltipTrigger>
+        <TooltipPortal>
+          <TooltipContent :side-offset="5">
+            <div class="bg-card shape-card text-content p-2">
+              <div class="flex select-none items-center">
+                Posted on {{ postTime }}
+              </div>
+              <div class="flex select-none items-center">
+                Updated on {{ updatedTime }}
+              </div>
+            </div>
+          </TooltipContent>
+        </TooltipPortal>
+      </TooltipRoot>
+    </TooltipProvider>
+  </div>
+</template>
