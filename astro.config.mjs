9209ceea223fe0791ea3c67e2061dc0b76964b8c
Chris Yang <yy4382@outlook.com>
2024-06-27 01:41:31 +0800

    fix: revert component auto import

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,8 +28,8 @@ export default defineConfig({
       imports: [
         {
           "astro-icon/components": ["Icon"],
-          "./src/utils/path": ["getPostPath"],
-          // "@comp/base/Card.astro": [["default", "Card"]],
+          "@utils/path": ["getPostPath"],
+          "@comp/base/Card.astro": [["default", "Card"]],
         },
         {
           from: "astro:content",
@@ -38,67 +38,6 @@ export default defineConfig({
         },
       ],
       defaultExportByFilename: true,
-      resolvers: [
-        async (name) => {
-          // 大驼峰，至少一个词
-          if (/^([A-Z][a-z]+){1,}$/.test(name)) {
-            if (commonSingleWordNonComponents.includes(name)) return;
-
-            console.log(name);
-            const autoImportComponents = await getComponentList([
-              [
-                "./src/components/**/*.astro",
-                { relativePath: "./src/components/" },
-              ],
-              "./src/layouts/*.astro",
-            ]);
-
-            const component = autoImportComponents.find(
-              (component) => component.name === name,
-            );
-
-            if (!component) return;
-
-            return {
-              importName: "default",
-              name,
-              path: component.file,
-            };
-
-            // // console.log(name);
-            // // 在单词间任意加斜杠/（每个当中可能加可能不加，穷举），检测路径是否存在，返回存在的路径
-            // const words = name.match(/[A-Z][a-z]+/g);
-            // const paths = [];
-            // for (let i = 0; i < Math.pow(2, words.length - 1); i++) {
-            //   const path = words.reduce((acc, word, index) => {
-            //     return acc + word + (i & (1 << index) ? "/" : "");
-            //   }, "./src/components/");
-            //   paths.push(path);
-            // }
-            // // console.log(paths);
-            // const realPath = (
-            //   await Promise.all(
-            //     paths.map(async (path) => {
-            //       const fullPath = path + ".astro";
-            //       const exists = await checkModuleExists(fullPath);
-            //       if (exists) return fullPath;
-            //       else return undefined;
-            //     }),
-            //   )
-            // )
-            //   .filter((path) => path)
-            //   .shift();
-            // if (realPath) {
-            //   // console.log(realPath);
-            //   return {
-            //     importName: "default",
-            //     path: realPath,
-            //     name,
-            //   };
-            // }
-          }
-        },
-      ],
     }),
   ],
   markdown: {
@@ -140,74 +79,3 @@ export default defineConfig({
     },
   },
 });
-
-// import { promises as fs } from "node:fs";
-// import { fileURLToPath } from "node:url";
-import path from "node:path";
-import fg from "fast-glob";
-
-// async function checkModuleExists(modulePath) {
-//   try {
-//     const __filename = fileURLToPath(import.meta.url);
-//     const __dirname = path.dirname(__filename);
-//     const fullPath = path.resolve(__dirname, modulePath);
-//     await fs.access(fullPath);
-//     return true;
-//   } catch {
-//     return false;
-//   }
-// }
-
-const commonSingleWordNonComponents = [
-  "Object",
-  "Math",
-  "Date",
-  "Number",
-  "Array",
-  "Set",
-];
-
-/**
- * Get component name list from dirs
- *
- * If ${dir}/Aa/bb/Cc.astro exists, then AaBbCc will be returned
- *
- * @example getComponentList(["./src/components/**", "./src/layouts"])
- *
- * @param {Array<Array<string>|string>} dirs
- * @returns {Promise<Array<{name: string, file: string}>>}
- */
-const getComponentList = async (dirs) => {
-  const names = await Promise.all(
-    dirs.map(async (dir) => {
-      const [globDir, { relativePath }] = Array.isArray(dir) ? dir : [dir, {}];
-
-      const files = await fg(globDir, { onlyFiles: true });
-
-      if (!relativePath)
-        return files.map((file) => ({
-          name: path.basename(file, path.extname(file)),
-          file,
-        }));
-      else
-        return files.map((file) => {
-          // convert file path to relative path
-          const relative = path.relative(relativePath, file);
-
-          // covert any path separator to camel case
-          return {
-            name: relative
-              .split(path.sep)
-              .map((part) => part[0].toUpperCase() + part.slice(1))
-              .join("")
-              .split(".")
-              .slice(0, -1)
-              .join(""),
-            file,
-          };
-        });
-    }),
-  );
-
-  return names.flat();
-};
