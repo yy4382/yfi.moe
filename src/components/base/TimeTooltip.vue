46ec018113cafb224cd41da8c268a4c0631e567a
Chris Yang <yy4382@outlook.com>
2024-06-30 15:50:49 +0800

    feat: use spring tooltip for time

--- src/components/base/TimeTooltip.vue
+++ src/components/base/TimeTooltip.vue
@@ -1,12 +1,5 @@
 <script setup lang="ts">
-import {
-  TooltipContent,
-  TooltipPortal,
-  TooltipProvider,
-  TooltipRoot,
-  TooltipTrigger,
-} from "radix-vue";
-import { Icon } from "@iconify/vue";
+import VTooltip from "./VTooltip.vue";
 
 defineProps<{
   postTime: string;
@@ -15,25 +8,17 @@ defineProps<{
 </script>
 
 <template>
-  <div>
-    <TooltipProvider>
-      <TooltipRoot>
-        <TooltipTrigger>
-          <slot></slot>
-        </TooltipTrigger>
-        <TooltipPortal>
-          <TooltipContent :side-offset="5">
-            <div class="bg-card shape-card text-content p-2">
-              <div class="flex select-none items-center">
-                Posted on {{ postTime }}
-              </div>
-              <div class="flex select-none items-center">
-                Updated on {{ updatedTime }}
-              </div>
-            </div>
-          </TooltipContent>
-        </TooltipPortal>
-      </TooltipRoot>
-    </TooltipProvider>
-  </div>
+  <VTooltip>
+    <slot />
+    <template #content>
+      <div class="bg-card shape-card text-content p-2 text-sm border-2 dark:border-gray-700">
+        <div class="flex select-none items-center">
+          Posted on {{ postTime }}
+        </div>
+        <div class="flex select-none items-center">
+          Updated on {{ updatedTime }}
+        </div>
+      </div>
+    </template>
+  </VTooltip>
 </template>
