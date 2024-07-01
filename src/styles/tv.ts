e36d44d40d2c31dabf0347792455a4c72924e65d
Chris Yang <yy4382@outlook.com>
2024-07-02 00:15:52 +0800

    refactor: move Card style to tv file

--- src/styles/tv.ts
+++ src/styles/tv.ts
@@ -27,3 +27,82 @@ export const popover = tv({
     icon: "size-6 text-primary",
   },
 });
+
+export const card = tv({
+  slots: {
+    base: "bg-card shape-card p-6",
+    heading: "text-heading",
+  },
+  variants: {
+    padding: {
+      xs: {
+        base: "p-2 sm:p-4",
+      },
+      sm: {
+        base: "p-4 sm:p-6",
+      },
+      md: {
+        base: "p-6 sm:p-8",
+      },
+    },
+    headSize: {
+      xl: {
+        heading: "text-xl font-medium tracking-tight",
+      },
+    },
+    useIndicator: {
+      true: {
+        heading:
+          "relative before:absolute before:-left-4 before:w-1 before:rounded-md before:bg-[var(--primary)] before:content-['']",
+      },
+    },
+    leftExtraPadding: {
+      true: {},
+    },
+  },
+  compoundVariants: [
+    {
+      headSize: "xl",
+      useIndicator: true,
+
+      class: { heading: "before:top-1 before:h-5" },
+    },
+    // indicator placement change due to padding
+    {
+      padding: "xs",
+      useIndicator: true,
+      class: { heading: "before:-left-2" },
+    },
+    {
+      padding: "sm",
+      useIndicator: true,
+      class: { heading: "before:-left-3" },
+    },
+
+    // leftExtraPadding changes
+    {
+      padding: "xs",
+      useIndicator: true,
+      leftExtraPadding: true,
+      class: { heading: "ml-1 sm:ml-2" },
+    },
+    {
+      padding: "sm",
+      useIndicator: true,
+      leftExtraPadding: true,
+      class: { heading: "ml-2 sm:ml-3" },
+    },
+    {
+      padding: "md",
+      useIndicator: true,
+      leftExtraPadding: true,
+      class: { heading: "ml-3 sm:ml-4" },
+    },
+  ],
+  defaultVariants: {
+    padding: "md",
+    headSize: "xl",
+    useIndicator: true,
+    leftExtraPadding: false,
+  },
+});
