4f31e3f1d7405c4fc2f172249e00fe29edf6041a
Chris Yang <yy4382@outlook.com>
2024-07-10 09:51:53 +0800

    feat: add heading anchor link

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,16 @@
-import type { Element } from "hast";
+import type { Element, Node } from "hast";
 import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
 import remarkGithubAlerts from "remark-github-alerts";
 import icon from "astro-icon";
 import remarkReadingTime from "./src/utils/remarkReadingTime.mjs";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeExtendedLinks from "rehype-extended-links";
+import rehypeSlug from "rehype-slug";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import autoImport from "unplugin-auto-import/astro";
-
+import { h } from "hastscript";
 // https://astro.build/config
 export default defineConfig({
   site: "https://yfi.moe/",
@@ -52,46 +54,40 @@ export default defineConfig({
   markdown: {
     remarkPlugins: [remarkGithubAlerts, remarkReadingTime],
     rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "append",
+          content(node: Element): Node | Node[] {
+            return h("span", { className: "heading-link" }, [
+              "#" + node.tagName.toUpperCase(),
+            ]);
+          },
+          properties: {
+            ariaHidden: true,
+            tabIndex: -1,
+            style: "border-bottom: 0px;",
+          },
+          headingProperties: {
+            className: "heading-linked",
+          },
+        },
+      ],
       [
         rehypeExtendedLinks,
         {
           preContent(node: Element): Element | undefined {
-            if (
-              node.children.some(
-                (child) => child.type === "element" && child.tagName === "img",
-              )
-            ) {
-              return undefined;
-            }
+            if (nodeHas(node, "img")) return undefined;
             const url = node.properties.href?.toString();
             if (!url) return undefined;
             const regex = /^(https?:\/\/)?(www\.)?github\.com\/.*/i;
             if (!regex.test(url)) return undefined;
-            return {
-              type: "element",
-              tagName: "span",
-              properties: {
-                className: ["i-mingcute-github-line"],
-              },
-              children: [],
-            };
+            return h("span", { className: ["i-mingcute-github-line"] });
           },
           content(node: Element): Element | undefined {
-            if (
-              node.children.some(
-                (child) => child.type === "element" && child.tagName === "img",
-              )
-            ) {
-              return undefined;
-            }
-            return {
-              type: "element",
-              tagName: "span",
-              properties: {
-                className: ["i-mingcute-external-link-line"],
-              },
-              children: [],
-            };
+            if (nodeHas(node, "img")) return undefined;
+            return h("span", { className: ["i-mingcute-external-link-line"] });
           },
         },
       ],
@@ -104,3 +100,13 @@ export default defineConfig({
     },
   },
 });
+
+const nodeHas = (node: Element, tagName: string | string[]): boolean => {
+  return node.children.some(
+    (child) =>
+      child.type === "element" &&
+      (typeof tagName === "string"
+        ? child.tagName === tagName
+        : tagName.includes(child.tagName)),
+  );
+};
