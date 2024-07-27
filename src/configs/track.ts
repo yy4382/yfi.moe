d062caf37a4c67645307fdd51f375fe5790fe703
Chris Yang <yy4382@outlook.com>
2024-07-28 01:43:15 +0800

    chore: use configurable umami and Google Analytics tracking scripts

---
+++ src/configs/track.ts
@@ -0,0 +1,7 @@
+export const umamiConfig = {
+  src: "https://analy.yfi.moe/script.js",
+  id: "3c89f8f6-9432-4ceb-a404-f22f2b0cf94d",
+  domains: ["yfi.moe"],
+};
+
+export const googleMeasurementId: string = "G-1MPEY5XJ9V";
