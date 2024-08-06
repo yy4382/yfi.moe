780895b98775d28a415f1f8901f57c70ff03fc75
Chris Yang <yy4382@outlook.com>
2024-08-07 01:32:37 +0800

    refactor: Update NoteCardContent and NoteList to use new API routes

---
+++ src/pages/api/note/[id].ts
@@ -0,0 +1,22 @@
+import type { APIRoute } from "astro";
+import { getPageContent } from "@libs/notion-client";
+
+export const GET: APIRoute = async ({ params, request: _ }) => {
+  const pageId = params.id ?? "";
+  const apiKey = import.meta.env.NOTION_API_KEY;
+  const pageContent = await getPageContent(pageId, {
+    apiKey,
+    accessControl: (resp) => {
+      const isValid =
+        "created_time" in resp &&
+        resp.parent.type === "database_id" &&
+        resp.parent.database_id.replace(/-/g, "") ===
+          import.meta.env.NOTION_NOTE_DATABASE_ID &&
+        resp.properties["Is Published"].type === "checkbox" &&
+        resp.properties["Is Published"].checkbox === true;
+
+      return isValid;
+    },
+  });
+  return new Response(JSON.stringify(pageContent));
+};
---
+++ src/pages/api/note/index.ts
@@ -0,0 +1,24 @@
+import type { APIRoute } from "astro";
+import { getEntries } from "@libs/notion-client";
+export type Page = {
+  title: string | undefined;
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export const GET: APIRoute = async () => {
+  const apiKey = import.meta.env.NOTION_API_KEY;
+  const databaseId = import.meta.env.NOTION_NOTE_DATABASE_ID;
+
+  const pages = await getEntries(databaseId, {
+    apiKey,
+    filter: {
+      property: "Is Published",
+      checkbox: {
+        equals: true,
+      },
+    },
+  });
+  return new Response(JSON.stringify(pages));
+};
