ba201059eb85101292af49bdb19aa6e79d627997
Chris Yang <yy4382@outlook.com>
2024-08-07 14:12:14 +0800

    feat: use compact list and skeleton

---
+++ src/components/modules/CompactEntryList/index.ts
@@ -0,0 +1,7 @@
+export * from "./CompactEntryList.astro";
+export { default as CompactEntryList } from "./CompactEntryList.astro";
+export * from "./CompactEntryListYear.astro";
+export { default as CompactEntryListYear } from "./CompactEntryListYear.astro";
+export * from "./CompactEntryListItem.astro";
+export { default as CompactEntryListItem } from "./CompactEntryListItem.astro";
+export { default as CompactEntryListSkeleton } from "./CompactEntryListSkeleton.astro";
--- src/libs/notion-client/extractMetaFromPage.ts
+++ src/libs/notion-client/extractMetaFromPage.ts
@@ -1,6 +1,6 @@
 import type { PageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 export type PageMeta = {
-  title: string | undefined;
+  title: string;
   id: string;
   createdAt: string;
   updatedAt: string;
@@ -9,18 +9,20 @@ export default function (page: PageObjectResponse): PageMeta {
   const title = findTitle(page.properties);
   return {
     title,
-    id: page.id,
+    id: page.id.replace(/-/g, ""),
     createdAt: page.created_time,
     updatedAt: page.last_edited_time,
   };
 }
 
 const findTitle = (titleProp: PageObjectResponse["properties"]) => {
-  return Object.entries(titleProp)
-    .map(([_, value]) => {
-      if (value.type === "title") {
-        return value.title[0].plain_text;
-      }
-    })
-    .find((value) => value !== undefined);
+  return (
+    Object.entries(titleProp)
+      .map(([_, value]) => {
+        if (value.type === "title") {
+          return value.title[0].plain_text;
+        }
+      })
+      .find((value) => value !== undefined) ?? ""
+  );
 };
