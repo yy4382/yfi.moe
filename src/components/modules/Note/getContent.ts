a3b04d1f77e83ee7c8af8a8d1f2e16f9c337506e
Chris Yang <yy4382@outlook.com>
2024-08-10 23:25:17 +0800

    refactor: note data structure

---
+++ src/components/modules/Note/getContent.ts
@@ -0,0 +1,46 @@
+import { getPageContent } from "@libs/notion-client";
+import {
+  NOTION_NOTE_DATABASE_ID as databaseId,
+  NOTION_API_KEY as apiKey,
+} from "astro:env/server";
+import type { GetPageResponse } from "@notionhq/client/build/src/api-endpoints";
+
+type ResponseData = Awaited<ReturnType<typeof getPageContent>>;
+
+export default async function getNoteContent(pageId: string): Promise<{
+  data: ResponseData | null;
+  error: string | null;
+  code: number;
+}> {
+  try {
+    const data = await getPageContent(pageId, { apiKey, accessControl });
+    return { data, error: null, code: 200 };
+  } catch (err) {
+    if (
+      err instanceof Error &&
+      err.cause &&
+      typeof err.cause === "object" &&
+      "code" in err.cause
+    ) {
+      if (err.cause.code === "ACCESS_DENIED") {
+        return { data: null, error: "Access Denied", code: 403 };
+      }
+      if (err.cause.code === "NOT_A_PAGE") {
+        return { data: null, error: "Invalid Page ID", code: 400 };
+      }
+    }
+    const error = err instanceof Error ? err.message : "An error occurred";
+    return { data: null, error, code: 500 };
+  }
+}
+
+const accessControl = (resp: GetPageResponse) => {
+  const isValid =
+    "created_time" in resp &&
+    resp.parent.type === "database_id" &&
+    resp.parent.database_id.replace(/-/g, "") === databaseId &&
+    resp.properties["Is Published"].type === "checkbox" &&
+    resp.properties["Is Published"].checkbox === true;
+
+  return isValid;
+};
