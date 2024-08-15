82925a6f80a45a4a60826086b18d7c15ed6de000
Chris Yang <yy4382@outlook.com>
2024-08-16 04:34:39 +0800

    test

--- app/blog/get-article.sh
+++ app/blog/get-article.sh
@@ -3,5 +3,6 @@ if [ -d "src/content/post" ]; then
   git pull
   cd ../../..
 else
-  git clone "https://$ARTICLE_PAT@github.com/yy4382/blog-posts.git" --depth 1 --branch main --single-branch --quiet src/content/post 
+  echo cloning using $ARTICLE_PAT
+  git clone "https://$ARTICLE_PAT@github.com/yy4382/blog-posts.git" --depth 1 --branch main --single-branch --quiet src/content/post
 fi
