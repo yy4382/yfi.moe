5e61b1460c1e69ce305c51bf90bc389974e7b6b3
Chris Yang <yy4382@outlook.com>
2024-08-16 17:13:11 +0800

    build: use turbo loose mode

--- app/blog/get-article.sh
+++ app/blog/get-article.sh
@@ -3,6 +3,5 @@ if [ -d "src/content/post" ]; then
   git pull
   cd ../../..
 else
-  echo cloning using $ARTICLE_PAT
   git clone "https://$ARTICLE_PAT@github.com/yy4382/blog-posts.git" --depth 1 --branch main --single-branch --quiet src/content/post
 fi
