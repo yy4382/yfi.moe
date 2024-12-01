0cc88adbce33fab29908be039bc44fb7342a9fd9
Chris Yang <yy4382@outlook.com>
2024-12-01 00:25:06 -0800

    refactor(blog): remove unused env

--- app/blog/astro.config.ts
+++ app/blog/astro.config.ts
@@ -103,14 +103,6 @@ export default defineConfig({
         access: "secret",
         optional: true,
       }),
-      OG_IMG_ENDPOINT: envField.string({
-        context: "server",
-        access: "public",
-      }),
-      OG_IMG_TOKEN: envField.string({
-        context: "server",
-        access: "secret",
-      }),
     },
   },
   devToolbar: {
