45642bcab7c1fb0a9059bf60eea7fcf6e720f6e7
Chris Yang <yy4382@outlook.com>
2024-12-08 13:16:14 -0800

    feat: rewrite Achieve page in react for better animation

---
+++ app/blog/src/components/ui/CompactEntryList/CompactEntryList.tsx
@@ -0,0 +1,125 @@
+import React, { Fragment } from "react";
+import { card, tvButton } from "@styles/tv";
+import { motion } from "motion/react";
+
+const springOpt = {
+  type: "spring",
+  visualDuration: 0.35,
+  bounce: 0.5,
+} as const;
+
+const BASE_DELAY = 0.1;
+const INTERVAL = 0.1;
+
+type CompactEntryListProps = {
+  keyword?: string;
+  rows: {
+    year: string;
+    items: {
+      title: string;
+      href: string;
+      children: React.ReactNode;
+    }[];
+  }[];
+};
+
+export const CompactEntryList: React.FC<CompactEntryListProps> = ({
+  keyword,
+  rows,
+}: CompactEntryListProps) => {
+  return (
+    <motion.div
+      className={card().base()}
+      initial={{ y: 50, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{
+        default: springOpt,
+        opacity: { duration: 0.5, ease: "linear" },
+      }}
+    >
+      {keyword && (
+        <motion.h1
+          className={card().heading()}
+          initial={{ y: 20 }}
+          animate={{ y: 0 }}
+          transition={{
+            ...springOpt,
+            delay: BASE_DELAY,
+          }}
+        >
+          {keyword}
+        </motion.h1>
+      )}
+      {rows.map((row, yearIndex) => (
+        <Fragment key={row.year}>
+          <motion.div
+            initial={{ y: 50 }}
+            animate={{ y: 0 }}
+            transition={{
+              ...springOpt,
+              delay: BASE_DELAY + yearIndex * INTERVAL,
+            }}
+          >
+            <CompactEntryListYear year={row.year} />
+          </motion.div>
+          <motion.div
+            initial={{ y: 50 }}
+            animate={{ y: 0 }}
+            transition={{
+              ...springOpt,
+              delay: BASE_DELAY + yearIndex * INTERVAL + INTERVAL / 2,
+            }}
+          >
+            <CompactEntryListItems items={row.items} />
+          </motion.div>
+        </Fragment>
+      ))}
+    </motion.div>
+  );
+};
+
+type CompactEntryListYearProps = {
+  year: string;
+};
+
+const CompactEntryListYear: React.FC<CompactEntryListYearProps> = ({
+  year,
+}) => {
+  return (
+    <div className="pointer-events-none relative h-12 select-none sm:h-20">
+      {/* https://stackoverflow.com/a/73146972 */}
+      <span className="absolute top-4 -left-4 text-8xl leading-none font-bold text-transparent opacity-10 [-webkit-text-stroke:4px_theme('colors.content.200')] sm:text-[9rem]">
+        {year}
+        <span className="pointer-events-none absolute left-0 text-card [-webkit-text-stroke:0]">
+          {year}
+        </span>
+      </span>
+    </div>
+  );
+};
+
+type CompactEntryListItemsProps = {
+  items: CompactEntryListProps["rows"][number]["items"];
+};
+
+const CompactEntryListItems: React.FC<CompactEntryListItemsProps> = ({
+  items,
+}) => {
+  return (
+    <ul className="timeline-dot -ml-3 flex flex-col md:ml-0">
+      {items.map((item) => (
+        <li
+          className={"timeline-dot-item group py-2 " + tvButton()}
+          key={item.title}
+        >
+          <a className="flex flex-col gap-1" href={item.href}>
+            <span className="truncate text-left text-sm text-heading transition group-hover:translate-x-1 md:text-base">
+              {item.title}
+            </span>
+            {item.children}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
--- app/blog/src/components/ui/CompactEntryList/index.ts
+++ app/blog/src/components/ui/CompactEntryList/index.ts
@@ -1,6 +1,22 @@
-export * from "./CompactEntryList.astro";
-export { default as CompactEntryList } from "./CompactEntryList.astro";
-export * from "./CompactEntryListYear.astro";
-export { default as CompactEntryListYear } from "./CompactEntryListYear.astro";
-export * from "./CompactEntryListItem.astro";
-export { default as CompactEntryListItem } from "./CompactEntryListItem.astro";
+export * from "./CompactEntryList";
+
+export function groupingEntries<T>(posts: T[], getYear: (arg0: T) => number) {
+  const groupedPosts = posts.reduce((grouped: Record<string, T[]>, post: T) => {
+    const year = getYear(post);
+    if (!grouped[year]) {
+      grouped[year] = [];
+    }
+    grouped[year].push(post);
+    return grouped;
+  }, {});
+
+  // convert the object to an array
+  const groupedPostsArray = Object.keys(groupedPosts).map((key) => ({
+    year: key,
+    posts: groupedPosts[key],
+  }));
+
+  // sort years by latest first
+  groupedPostsArray.sort((a, b) => parseInt(b.year) - parseInt(a.year));
+  return groupedPostsArray;
+}
