e00334ae18cdf0735c987457ed79a6e0495a0085
Chris Yang <yy4382@outlook.com>
2025-02-23 01:15:13 +0800

    refactor: implement Post Lists, remove unused files

--- app/blog/src/utils/remToPixel.ts
+++
@@ -1,7 +0,0 @@
-export default function remToPixel(rem: number): number {
-  if (!globalThis.window) return rem * 16;
-  const rootFontSize = parseFloat(
-    window.getComputedStyle(document.documentElement).fontSize,
-  );
-  return rem * rootFontSize;
-}
