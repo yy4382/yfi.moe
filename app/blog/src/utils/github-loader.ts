5482ef10921d046502150275496d48324b70a4da
Chris Yang <yy4382@outlook.com>
2025-02-23 00:18:07 +0800

    feat: integrate ofetch for GitHub API requests

--- app/blog/src/utils/github-loader.ts
+++ app/blog/src/utils/github-loader.ts
@@ -5,6 +5,7 @@ import {
   parseFrontmatter,
   type MarkdownProcessor,
 } from "@astrojs/markdown-remark";
+import { ofetch } from "ofetch";
 
 type GetRepoContentDir = components["schemas"]["content-directory"];
 type GetRepoContentFile = components["schemas"]["content-file"];
@@ -31,41 +32,62 @@ export function githubLoader(inputOptions: Options): Loader {
     pat: undefined,
   };
 
-  const fetchWithHeader = (...args: Parameters<typeof fetch>) => {
-    try {
-      return fetch(args[0], {
-        ...args[1],
-        headers: {
-          ...args[1]?.headers,
-          Authorization: `Bearer ${pat}`,
-          Accept: "application/vnd.github+json",
-          "X-GitHub-Api-Version": "2022-11-28",
-        },
-      });
-    } catch (e) {
-      throw new Error(
-        "Failed to fetch with header in github loader",
-        e as Error,
-      );
-    }
-  };
+  // const fetchWithHeader = (...args: Parameters<typeof fetch>) => {
+  //   try {
+  //     return fetch(args[0], {
+  //       ...args[1],
+  //       headers: {
+  //         ...args[1]?.headers,
+  //         Authorization: `Bearer ${pat}`,
+  //         Accept: "application/vnd.github+json",
+  //         "X-GitHub-Api-Version": "2022-11-28",
+  //       },
+  //     });
+  //   } catch (e) {
+  //     throw new Error(
+  //       "Failed to fetch with header in github loader",
+  //       e as Error,
+  //     );
+  //   }
+  // };
+
+  const fetcher = ofetch.create({
+    headers: {
+      Authorization: `Bearer ${pat}`,
+      Accept: "application/vnd.github+json",
+      "X-GitHub-Api-Version": "2022-11-28",
+    },
+    retry: 3,
+    retryDelay: 500,
+    timeout: 5000,
+  });
 
   return {
     name: "github-loader",
     load: async (ctx) => {
-      const processor = await createMarkdownProcessor(ctx.config.markdown);
+      // check if last sha is the same as current sha
+      const curSha = await fetchRefSha(options, fetcher, ctx);
+      const lastSha = ctx.meta.get("lastSha");
+      if (curSha !== lastSha) {
+        await syncContent();
+        ctx.meta.set("lastSha", curSha);
+      } else {
+        ctx.logger.info("No new commits, skipping sync");
+      }
 
-      const syncContent = async () => {
+      async function syncContent() {
+        const processor = await createMarkdownProcessor(ctx.config.markdown);
+
         const untouched = new Set(ctx.store.keys());
 
-        const dirFiles = (await fetchDir(options, fetchWithHeader, ctx)).filter(
+        const dirFiles = (await fetchDir(options, fetcher, ctx)).filter(
           (file) => file.type === "file",
         );
         const files = await Promise.all(
           dirFiles.map(async (file) => {
             const fileResp = await fetchFileContent(
               { ...options, path: file.path },
-              fetchWithHeader,
+              fetcher,
               ctx,
             );
             return processFile(fileResp, ctx, processor);
@@ -79,14 +101,6 @@ export function githubLoader(inputOptions: Options): Loader {
 
         // removed files that no longer in repo
         untouched.forEach((id) => ctx.store.delete(id));
-      };
-      const curSha = await fetchRefSha(options, fetchWithHeader, ctx);
-      const lastSha = ctx.meta.get("lastSha");
-      if (curSha !== lastSha) {
-        await syncContent();
-        ctx.meta.set("lastSha", curSha);
-      } else {
-        ctx.logger.info("No new commits, skipping sync");
       }
     },
   };
@@ -94,7 +108,7 @@ export function githubLoader(inputOptions: Options): Loader {
 
 async function fetchRefSha(
   options: Omit<Options, "pat">,
-  fetcher: typeof fetch,
+  fetcher: typeof ofetch,
   ctx: LoaderContext,
 ) {
   const { owner, repo, ref } = options;
@@ -102,59 +116,44 @@ async function fetchRefSha(
     `repos/${owner}/${repo}/git/ref/${ref}`,
     "https://api.github.com",
   );
-  const resp = await fetcher(url);
-  if (!resp.ok) {
-    ctx.logger.error(await resp.text());
-    throw new Error("Failed to fetch SHA of ref " + ref);
-  }
-  const data = (await resp.json()) as GetRef;
+  const data: GetRef = await fetcher(url.toString());
   return data.object.sha;
 }
 
 async function fetchDir(
   options: Omit<Options, "pat">,
-  fetcher: typeof fetch,
+  fetcher: typeof ofetch,
   ctx: LoaderContext,
-) {
+): Promise<GetRepoContentDir> {
   const { owner, repo, path, ref } = options;
   const url = new URL(
     `repos/${owner}/${repo}/contents/${path}`,
     "https://api.github.com",
   );
   if (ref) url.searchParams.set("ref", ref);
-  const resp = await fetcher(url);
-  if (!resp.ok) {
-    ctx.logger.error(await resp.text());
-    throw new Error("Failed to fetch path " + path);
-  }
-  const data = await resp.json();
+  const data = await fetcher(url.toString());
   if (!Array.isArray(data)) {
     throw new Error("Not a directory");
   }
-  return data as GetRepoContentDir;
+  return data;
 }
 
 async function fetchFileContent(
   options: Omit<Options, "pat">,
-  fetcher: typeof fetch,
+  fetcher: typeof ofetch,
   ctx: LoaderContext,
-) {
+): Promise<GetRepoContentFile> {
   const { owner, repo, path, ref } = options;
   const url = new URL(
     `repos/${owner}/${repo}/contents/${path}`,
     "https://api.github.com",
   );
   if (ref) url.searchParams.set("ref", ref);
-  const resp = await fetcher(url);
-  if (!resp.ok) {
-    ctx.logger.error(await resp.text());
-    throw new Error("Failed to fetch file " + path);
-  }
-  const data = await resp.json();
+  const data = await fetcher(url.toString());
   if (!("content" in data)) {
     throw new Error(`The file at path ${path} does not contain content`);
   }
-  return data as GetRepoContentFile;
+  return data;
 }
 
 async function processFile(
