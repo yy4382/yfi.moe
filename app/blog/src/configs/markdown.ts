bcb83c80e285900f32637fdcef8ff7cde98dda9c
Chris Yang <yy4382@outlook.com>
2025-06-09 22:13:29 +0800

    chore: delete unused files

--- app/blog/src/configs/markdown.ts
+++
@@ -1,8 +0,0 @@
-export const linkIcons = (): [string, RegExp][] => [
-  ["i-mingcute-github-line", /^(https?:\/\/)?(www\.)?github\.com\/.*/i],
-  [
-    "i-mingcute-twitter-line",
-    /^(https?:\/\/)?(www\.)?(twitter|x|fxtwitter|fixupx)\.com\/.*/i,
-  ],
-  ["i-mingcute-sword-line", /^(https?:\/\/)?([\w-]+\.)*yfi\.moe(\/.*)?$/],
-];
