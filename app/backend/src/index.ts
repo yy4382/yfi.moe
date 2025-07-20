4ec8bd83eb2ac07fcdbc8e38660ddab3d300e947
Chris Yang <yy4382@outlook.com>
2025-07-20 21:02:29 +0800

    chore: fix typo

--- app/backend/src/index.ts
+++ app/backend/src/index.ts
@@ -2,13 +2,16 @@ import { Elysia } from "elysia";
 import { node } from "@elysiajs/node";
 import { migrate } from "drizzle-orm/libsql/migrator";
 import { db } from "./db/instance";
+import { comments } from "./modules/comments";
 
 migrate(db, { migrationsFolder: "./drizzle" });
 
 const app = new Elysia({ adapter: node(), prefix: "/api/v1" })
-  .get("/", () => "Hello Elysia")
+  .use(comments)
   .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
+
+export type App = typeof app;
