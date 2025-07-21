1777a5e76790ff9c48a481032874685089b8b2db
Chris Yang <yy4382@outlook.com>
2025-07-21 17:18:53 +0800

    chore(backend): update bundling config

--- app/backend/tsdown.config.ts
+++ app/backend/tsdown.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from "tsdown/config";
 
-export default defineConfig({
-  entry: ["src/index.ts", "src/client.ts"],
-  dts: {
-    sourcemap: true,
-  },
-  format: "esm",
-  platform: "node",
-  unbundle: true,
-});
+export default [
+  defineConfig({
+    entry: ["src/index.ts"],
+    platform: "node",
+  }),
+];
