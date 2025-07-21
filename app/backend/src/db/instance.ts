bc9d193fa71d459a2a8065bed2dae503076ba102
Chris Yang <yy4382@outlook.com>
2025-07-21 17:18:01 +0800

    feat(backend): validate runtime env

--- app/backend/src/db/instance.ts
+++ app/backend/src/db/instance.ts
@@ -1,4 +1,5 @@
 import { drizzle } from "drizzle-orm/libsql";
 import * as schema from "./schema";
+import { config } from "@/config";
 
-export const db = drizzle(process.env.DB_FILE_NAME!, { schema });
+export const db = drizzle(config.dbFileName, { schema });
