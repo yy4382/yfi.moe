4baa7966e0cd082b18fad74c84127d2351c1a2fa
Chris Yang <yy4382@outlook.com>
2025-07-23 23:29:00 +0800

    chore(blog): use neon database

--- app/blog/src/env.ts
+++ app/blog/src/env.ts
@@ -10,7 +10,7 @@ export const env = createEnv({
     UPSTASH_REDIS_REST_URL: z.url(),
     UPSTASH_REDIS_REST_TOKEN: z.string(),
     BETTER_AUTH_SECRET: z.string(),
-    DB_FILE_NAME: z.string(),
+    DATABASE_URL: z.string(),
 
     // auth
     GITHUB_CLIENT_ID: z.string(),
@@ -36,7 +36,7 @@ export const env = createEnv({
     UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
     UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
     BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
-    DB_FILE_NAME: process.env.DB_FILE_NAME,
+    DATABASE_URL: process.env.DATABASE_URL,
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
     EMAIL_NOTIFICATION_ENABLED: process.env.EMAIL_NOTIFICATION_ENABLED,
