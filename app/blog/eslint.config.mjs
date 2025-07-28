0b7e8cf5e4c6d871fa23d4b231f56c34b82b5882
Chris Yang <yy4382@outlook.com>
2025-07-28 19:04:51 +0800

    fix(blog): add typescript type check eslint rules

--- app/blog/eslint.config.mjs
+++ app/blog/eslint.config.mjs
@@ -3,6 +3,7 @@ import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 import pluginQuery from "@tanstack/eslint-plugin-query";
 import reactHooks from "eslint-hooks-rc";
+import tseslint from "typescript-eslint";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,7 +12,7 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+const eslintConfig = tseslint.config(
   ...pluginQuery.configs["flat/recommended"],
   {
     files: ["**/*.{ts,tsx,js,jsx}"],
@@ -20,7 +21,27 @@ const eslintConfig = [
       "react-hooks-rc/react-compiler": "error",
     },
   },
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: ["next/core-web-vitals"],
+  }),
+  [
+    ...tseslint.configs.recommendedTypeChecked,
+    {
+      name: "typescript-eslint/custom-added",
+      rules: {
+        "@typescript-eslint/no-unused-vars": "warn",
+        "@typescript-eslint/no-unused-expressions": "warn",
+      },
+    },
+  ],
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+);
 
 export default eslintConfig;
