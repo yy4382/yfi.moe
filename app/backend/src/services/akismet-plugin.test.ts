5f18c9eaa81df32d2a465b7f6bba1867995892d6
Chris Yang <yy4382@outlook.com>
2025-08-11 16:34:40 +0800

    chore: fix eslint error

--- app/backend/src/services/akismet-plugin.test.ts
+++ app/backend/src/services/akismet-plugin.test.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { describe, it, expect, vi } from "vitest";
 
 // Mock the akismet service
