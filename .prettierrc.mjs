536ab5d8ac234b8e5f322f0dc3179011ccda7e92
Chris Yang <yy4382@outlook.com>
2025-09-28 04:13:21 +0800

    feat(prettier): migrate to new Prettier configuration with plugins for Astro, tailwind and import sorting

---
+++ .prettierrc.mjs
@@ -0,0 +1,18 @@
+/** @type {import("prettier").Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+export default {
+  plugins: [
+    "prettier-plugin-astro",
+    "@trivago/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+  tailwindStylesheet: "./app/blog-astro/src/styles/globals.css",
+  importOrder: ["<THIRD_PARTY_MODULES>", "^@repo/(.*)$", "^@/(.*)$", "^[./]"],
+};
