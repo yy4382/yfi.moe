c733591f0a98c51c39a9c3616eda3a201a623efe
Chris Yang <yy4382@outlook.com>
2025-10-04 01:33:57 +0800

    fix(tests): update vitest config to handle environment variables and improve test execution

--- app/blog-astro/vitest.config.ts
+++ app/blog-astro/vitest.config.ts
@@ -1,13 +1,27 @@
-import type { ViteUserConfig } from "astro";
-import { getViteConfig } from "astro/config";
+// /// <reference types="vitest" />
+/* 
+the three slashes won't work here because vitest is calling vite 7, while
+  astro is calling vite 6, so we need to manually call defineConfig instead 
+*/
+import { envField, getViteConfig } from "astro/config";
 import { defineConfig } from "vitest/config";
 
 export default getViteConfig(
   defineConfig({
     test: {},
-  }) as ViteUserConfig,
+  }),
   {
     // override to empty env, allowing tests to run without env variables
-    env: {},
+    env: {
+      schema: {
+        // for unknown reasons, we must set envs that are "client and not optional" to optional to stop astro
+        // from making them required and failing tests
+        WALINE_URL: envField.string({
+          optional: true,
+          context: "client",
+          access: "public",
+        }),
+      },
+    },
   },
 );
