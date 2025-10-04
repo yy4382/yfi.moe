9db5f688659aac78442d8067a6815910d2bed2a3
Chris Yang <yy4382@outlook.com>
2025-10-04 20:34:14 +0800

    chore: add "about" mdx page

--- app/blog-astro/src/components/modules/comments/comment.tsx
+++ app/blog-astro/src/components/modules/comments/comment.tsx
@@ -1,7 +1,6 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { WALINE_URL } from "astro:env/client";
 import { useEffect, useState } from "react";
-import { Toaster } from "sonner";
 import CommentYuline from "@repo/comment";
 import { queryClient } from "@/lib/query-client";
 
