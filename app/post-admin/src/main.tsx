7a00db3f28844be60e181271358d42cc1718c6db
Chris Yang <yy4382@outlook.com>
2025-12-30 13:42:03 +0800

    refactor: reuse markdown styles

--- app/post-admin/src/main.tsx
+++ app/post-admin/src/main.tsx
@@ -1,3 +1,4 @@
+import { StyleProvider } from "@ant-design/cssinjs";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ConfigProvider, App as AntdApp } from "antd";
 import { StrictMode } from "react";
@@ -18,13 +19,15 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ConfigProvider>
-        <AntdApp>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </AntdApp>
-      </ConfigProvider>
+      <StyleProvider layer>
+        <ConfigProvider>
+          <AntdApp>
+            <BrowserRouter>
+              <App />
+            </BrowserRouter>
+          </AntdApp>
+        </ConfigProvider>
+      </StyleProvider>
     </QueryClientProvider>
   </StrictMode>,
 );
